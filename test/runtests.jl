af5a6f3db6497d5490b22a6870fc01099ac66c6b
anand <anandj@uchicago.edu>
2021-01-05 14:25:51 -0800

    remove jl 1.0 from CI. update version

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,25 +42,3 @@ end
         end
     end
 end
-
-    # @testset "allocating" begin
-        
-    #     @test 3*4 == 12    
-    # end
-
-
-
-
-        # lv_euler = mysolve_inplace(eulers_inplace_f!, lotka!, u0, tspan, dt; p=p)
-        # lv_mid = mysolve_inplace(midpoint_inplace_f!, lotka!, u0, tspan, dt; p=p)
-        # lv_rk4_inp = mysolve_inplace(rk4_inplace_f!, lotka!, u0, tspan, dt; p=p)
-        # # fix to use mysolve_inplace
-        # lv_dp5 = mysolve(dp5!, lotka, u0, tspan, dt; p=p)
-        
-        # # lv_prob = ODEProblem(lotka!, u0, tspan, p)
-        # # lv_de = solve(lv_prob, saveat=ts)
-        # # mysols = [lv_euler, lv_mid, lv_rk4_inp, lv_dp5]    
-        # # myerrs = sum.(abs2, reference_soln .- [lv_eul_err, lv_mid_err, lv_rk4_err, lv_dp5_err])
-        
-        # # tests that error is decreasing as solver order is increasing
-        # @test issorted(myerrs, rev=true)
